a723500d022f8c9b49f0b174f840b216401bc682
Cortney Lane <cdlane124@gmail.com>
2017-02-18 21:17:05 -0800

    screw it this app is using react now

--- app/script.js
+++ app/script.js
@@ -1,3 +1,7 @@
 'use strict';
 
-const $ = require('jquery');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+
+ReactDOM.render(<App />, document.getElementById('app'));
